37bbac43634b45a9f9137e144a70f85c41553181
Jeffrey Gisin <jgisin@gmail.com>
2016-01-12 12:03:40 -0700

    added queue

--- queue.rb
+++ queue.rb
@@ -1,5 +1,35 @@
 class Queue
 
+attr_accessor :array
 
+  @@position = 0
+  
+  def initialize
+    @array = []
+  end
+
+  def enqueue(data)
+    @array[@array.length] = data
+  end
+
+  def dequeue
+    @array -= [@array[0]]
+  end
+
+  def peek
+    @array[@@position]
+  end
+
+  def next
+    @@position += 1
+  end
+
+  def empty?
+    if @array == [] || @array == nil
+      return true
+    else
+      return false
+    end
+  end
 
 end
