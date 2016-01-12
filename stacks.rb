1cd4327a82f987c1ae2f0f93d6e89ebe31c3844b
Jeffrey Gisin <jgisin@gmail.com>
2016-01-12 12:02:38 -0700

    Finished stack, working on queue

--- stacks.rb
+++ stacks.rb
@@ -1,20 +1,37 @@
 class Stack
 
+attr_accessor :array
+
+	@@position = 0
+	
 	def initialize
-		@array
+		@array = []
 	end
 
-	def push()
-
+	def push(data)
+		@array[@array.length] = data
 	end
 
 	def pop
+		@array -= [@array[@array.length - 1]]
 	end
 
 	def peek
+		@array[@@position]
+	end
+
+	def next
+		@@position += 1
 	end
 
 	def empty?
+		if @array == [] || @array == nil
+			return true
+		else
+			return false
+		end
 	end
 
 end
+
+
